5624ef51e6e53f032d2e2f612b0cbe1a0ccf8d87
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-07-31 09:51:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
   <h1>Hello, World! I am new in Github and Heroku!</h1>
   
   <footer>
+   <p>I am Regine Megg U. Mata</p>
+   <p>I am an IT Freshman of the University of San Jose-Recoletos</p>
     <p>I am a student of Mr. Lee Cuizon in Web Development 1</p>
+   <p>I am an IT Freshman of the University of San Jose-Recoletos
   </footer>
 
 
